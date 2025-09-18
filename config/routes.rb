8b5bc6a44b2bd658b0f1ae0302ee3aad3d540471
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-09-18 18:06:17 +0900

    画面表示の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'novels/index'
-  get 'novels/show'
-  get 'novels/new'
-  get 'novels/create'
-  get 'novels/edit'
-  get 'novels/update'
-  get 'novels/destroy'
   root 'novels#index'
   devise_for :users
   resources :novels
